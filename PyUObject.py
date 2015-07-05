12308a72674fa7d568761740e3fc6a1d125de585
fancyzero <FancyZero@FancyiMac.local>
2015-07-06 00:33:11 +0800

    structure conversion done, need more work to convert properties
    can parse properties recursively now

--- PyUObject.py
+++ PyUObject.py
@@ -1,65 +1,89 @@
-class Object:
-	def __init__(self):
-		self.cls = ""
-		self.name = ""
-		self.children = []
-		self.properties = {}
-		self.parent = None
+import sys
+intenter = "   "
+intent_str = "   "
 
-	def attach_to(self, new_parent):
-		if self == new_parent:
-			return
-		self.detach_from_parent()	
-		if new_parent != None:
-			new_parent.children.append(self)
-		self.parent = new_parent
+output_device=sys.stdout
+class PyUObject:
 
-	def detach_from_parent(self):
-		if self.parent != None:
-			self.parent.children.remove(self)
-		self.parent = None	
-		
-	def output_propertier(self,depth):
-		for k,v in self.properties.iteritems():
-			print intent_str * depth + k+"=" + v
+    def __init__(self):
+        self.cls = ""
+        self.name = ""
+        self.children = []
+        self.properties = {}
+        self.parent = None
 
-	def output_declare(self,depth):
-		intent = intent_str*depth
-		str = intent +  "Begin Object"
-		if self.name:
-			str += " Name="+self.name
-		if self.cls:
-			str += " CLass="+self.cls
-		print str
-		for child in self.children:
-			child.output_declare(depth+1)
-		print intent + "End Object"
-		
-	def output_detail(self,depth):
-		intent = intent_str*depth
-		str = intent +  "Begin Object"
-		if self.name:
-			str += " Name="+self.name
-		if self.cls:
-			str += " CLass="+self.cls
-		print str
-		self.print_propertier(depth+1)
-		for child in self.children:
-			child.output_detail(depth+1)
+    def attach_to(self, new_parent):
+        if self == new_parent:
+            return
+        self.detach_from_parent()
+        if new_parent != None:
+            new_parent.children.append(self)
+        self.parent = new_parent
 
-		print intent + "End Object"
+    def detach_from_parent(self):
+        if self.parent != None:
+            self.parent.children.remove(self)
+        self.parent = None
 
-#for debugging		
-	def debug_list_child(self):
-		for child in self.children:
-			print child.name
-			
-	def debug_output_struct(self, depth):
-		str = intenter * depth;
-		if self.name:
-			str += "name: " + self.name 
-		if self.cls:
-			str  += "<"+self.cls+">"
-		print str
-		for child in self.children:
-			child.output_struct(depth+1)
+    def output_properties(self, depth):
+        for k,v in self.properties.iteritems():
+            output_device.write(intent_str * depth )
+            self.output_properties_recursiv(k, v, depth)
+            output_device.write("\r\n")
+
+    def output_properties_recursiv(self, key, value, depth):
+        if value.__class__ == dict:
+            output_device.write( key + "=(")
+            need_comma = False
+            for k,v in value.iteritems():
+                if need_comma:
+                    output_device.write(",")
+                else:
+                    need_comma = True
+                self.output_properties_recursiv(k,v,depth)
+            output_device.write(")")
+        else:
+            output_device.write(  key + "=" + value )
+
+
+
+    #output
+    def output_declare(self, depth):
+        intent = intent_str * depth
+        str = intent + "Begin Object"
+        if self.name:
+            str += " Name=" + self.name
+        if self.cls:
+            str += " CLass=" + self.cls
+        output_device.write( str+"\r\n")
+        for child in self.children:
+            child.output_declare(depth + 1)
+        output_device.write( intent + "End Object\r\n")
+
+    def output_detail(self, depth):
+        intent = intent_str * depth
+        str = intent + "Begin Object"
+        if self.name:
+            str += " Name=" + self.name
+        if self.cls:
+            str += " CLass=" + self.cls
+        output_device.write( str+"\r\n")
+        self.output_properties(depth + 1)
+        for child in self.children:
+            child.output_detail(depth + 1)
+        output_device.write( intent + "End Object\r\n")
+
+    # for debugging
+    def debug_list_child(self):
+        for child in self.children:
+            output_device.write( child.name)
+
+    def debug_output_struct(self, depth):
+        str = intenter * depth;
+        if self.name:
+            str += "name: " + self.name
+        if self.cls:
+            str += "<" + self.cls + ">"
+        output_device.write( str)
+        for child in self.children:
+            child.output_struct(depth + 1)
--- milky.py
+++ milky.py
@@ -1,179 +1,134 @@
 import sys
+import PyUObject
 import re
 
-# repsent struct and intent level
-global intent
-global intent_str
-
+# represent structure and intent level when output
 intent_str = "   "
 intent = 0
 
 def add_intent():
-	intent = intent + 1
+    global intent
+    intent += 1
 
 def sub_intent():
-	intent = intent - 1
+    global intent
+    intent += -1
 
 def output(str):
-	print intent_str * intent + str
+    global intent_str
+    global intent
+    print intent_str * intent + str
 
-class Object:
-	def __init__(self):
-		self.cls = ""
-		self.name = ""
-		self.children = []
-		self.properties = {}
-		self.parent = None
 
-	def attach_to(self, new_parent):
-		if self == new_parent:
-			return
-		self.detach_from_parent()	
-		if new_parent != None:
-			new_parent.children.append(self)
-		self.parent = new_parent
-
-	def detach_from_parent(self):
-		if self.parent != None:
-			self.parent.children.remove(self)
-		self.parent = None	
-		
-	def output_propertier(self,depth):
-		for k,v in self.properties.iteritems():
-			print intent_str * depth + k+"=" + v
-
-	def output_declare(self,depth):
-		intent = intent_str*depth
-		str = intent +  "Begin Object"
-		if self.name:
-			str += " Name="+self.name
-		if self.cls:
-			str += " CLass="+self.cls
-		print str
-		for child in self.children:
-			child.output_declare(depth+1)
-		print intent + "End Object"
-		
-	def output_detail(self,depth):
-		intent = intent_str*depth
-		str = intent +  "Begin Object"
-		if self.name:
-			str += " Name="+self.name
-		if self.cls:
-			str += " CLass="+self.cls
-		print str
-		self.print_propertier(depth+1)
-		for child in self.children:
-			child.output_detail(depth+1)
-
-		print intent + "End Object"
-
-#for debugging		
-	def debug_list_child(self):
-		for child in self.children:
-			print child.name
-			
-	def debug_output_struct(self, depth):
-		str = intenter * depth;
-		if self.name:
-			str += "name: " + self.name 
-		if self.cls:
-			str  += "<"+self.cls+">"
-		print str
-		for child in self.children:
-			child.output_struct(depth+1)
-
-		
 def find_obj_by_class(root, class_name):
-	if root == None:
-		return None
-	if root.cls == class_name:
-		return root
-	for child in root.children:
-		ret = find_obj_by_class(child, class_name)
-		if ret != None:
-			return ret
+    if root == None:
+        return None
+    if root.cls == class_name:
+        return root
+    for child in root.children:
+        ret = find_obj_by_class(child, class_name)
+        if ret != None:
+            return ret
 
 #Reposition object in UE3 tree , to match UE4 partten
 def reposition_obj( root, obj ):
-	# rule 1 move all module under ParticleSystem Node 
-	if obj.cls.startswith("ParticleModule"):
-		new_parent = find_obj_by_class(root, "ParticleSystem")
-		obj.attach_to(new_parent)
+    # rule 1 move all module under ParticleSystem Node
+    if obj.cls.startswith("ParticleModule"):
+        new_parent = find_obj_by_class(root, "ParticleSystem")
+        obj.attach_to(new_parent)
 
 def fix_properties(obj):
-	for k in obj.properties.keys():
-		if k == "ObjectArchetype":
-			del obj.properties[k]
-		if k == "Name":
-			del obj.properties[k]
+    for k in obj.properties.keys():
+        if k == "ObjectArchetype":
+            del obj.properties[k]
+        if k == "Name":
+            del obj.properties[k]
 
-def extract_params ( line ):
-	params = re.findall(r'(\w+)=\"*(\w+)\"*', line)
-	pmap = {}
-	for k,v in params:
-		pmap[k]=v
-	return pmap
+
+def extract_params(line):
+    params = re.findall(r'(\w+)=\"*(\w+)\"*', line)
+    pmap = {}
+    for k, v in params:
+        pmap[k] = v
+    return pmap
+
+def parse_properties(line):
+    pos = line.find("=")
+    k = line[0:pos]
+    v = line[pos+1:]
+    starts_with_parenthsis = v.startswith("(")
+    have_assign = v.find("=") >= 0
+    if starts_with_parenthsis and have_assign:
+        v=v[1:-1]
+        subparray = re.findall("\w+=\(.*?\)|\w+=\".*?\"|\w+=.*?,|\w+=.*",v)
+        v={}
+
+        for subp in subparray:
+            if subp.endswith(","):#clean out ending ","
+                subp=subp[:-1]
+            subk, subv = parse_properties( subp )
+            v[subk]=subv
+    return k,v
+
 
 def read_object(obj,f):
-	child = None
-	while True:
-		line = f.readline()
-		line = line.strip()
-		if line == "":
-			return child
-		if line.startswith("Begin Object"):
-			child = Object()
-			params = extract_params(line)
-			child.name = params.get("Name")
-			child.cls = params.get("Class")
-			read_object(child,f)
-			child.attach_to(obj)
-		elif line.startswith("End Object"):
-			return obj
-		else:
-			if obj != None:
-				pos = line.find("=")
-				obj.properties[line[0:pos]] = line[pos+1:]
-	return child
+    child = None
+    while True:
+        line = f.readline()
+        line = line.strip()
+        if line == "":
+            return child
+        if line.startswith("Begin Object"):
+            child = PyUObject.PyUObject()
+            params = extract_params(line)
+            child.name = params.get("Name")
+            child.cls = params.get("Class")
+            read_object(child,f)
+            child.attach_to(obj)
+        elif line.startswith("End Object"):
+            return obj
+        else:
+            if obj != None:
+                k,v=parse_properties(line)
+                obj.properties[k] = v
+    return child
 
 def convert(root, obj):
-	reposition_obj(root, obj)
-	fix_properties(obj)
+    reposition_obj(root, obj)
+    fix_properties(obj)
 
 def get_all_objects(root):
-	ret = []
-	ret.append(root)
-	for child in root.children:
-		ret = ret + get_all_objects(child)
-	return ret
+    ret = []
+    ret.append(root)
+    for child in root.children:
+        ret = ret + get_all_objects(child)
+    return ret
 
 def list_struct(filename):
-	f = open(filename)
-	root = read_object(None,f)
-	root.output_struct(0)
+    f = open(filename)
+    root = read_object(None,f)
+    root.output_struct(0)
 
 def convert_file(filename):
-	f = open(filename)
-	root = read_object(None,f)
-	all_obj = get_all_objects(root)
-	for obj in all_obj:
-		convert(root,obj)
-	root.output_declare(0)
-	root.output_detail(0)
+    f = open(filename)
+    root = read_object(None,f)
+    all_obj = get_all_objects(root)
+    for obj in all_obj:
+        convert(root,obj)
+    root.output_declare(0)
+    root.output_detail(0)
 
 def main():
-	if len(sys.argv) < 2:
-		print " need file name"
-		exit()
-	filename = sys.argv[1]
-	convert_file(filename)
+    if len(sys.argv) < 2:
+        print " need file name"
+        exit()
+    filename = sys.argv[1]
+    convert_file(filename)
 
 main()
 
 
-		
 
-		
 
-	
+
+
